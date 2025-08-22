c7a9e8d38095a6472902c3ce89e90f548eafc373
Alexis-9 <alexis.munoz.9u@gmail.com>
2025-08-22 04:52:16 -0300

    Tp1Ej1

--- TP1/Ejercicio1.py
+++ TP1/Ejercicio1.py
@@ -21,6 +21,20 @@ def ingresar_valores():
     segundo = int(input("Ingrese el segundo número: "))
     tercero = int(input("Ingrese el tercer número: "))
 
+    bien=False
+    while bien==False:
+        if primero < 0:
+            print("No se permiten números negativos.")
+            primero = int(input("Ingrese el primer número: "))
+        elif segundo < 0:
+            print("No se permiten números negativos.")
+            segundo = int(input("Ingrese el segundo número: "))
+        elif tercero < 0:
+            print("No se permiten números negativos.")
+            tercero = int(input("Ingrese el tercer número: "))
+        else:
+            bien=True
+
     resultado = mayor(primero, segundo, tercero)
 
     if resultado == -1:

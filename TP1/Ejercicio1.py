647d39e822cdff8cd5107c3fe5cab6773878ac05
Alexis-9 <alexis.munoz.9u@gmail.com>
2025-09-03 04:32:22 -0300

    Tp1Ej7

--- TP1/Ejercicio1.py
+++ TP1/Ejercicio1.py
@@ -4,17 +4,15 @@ caso de no haber ninguno. No utilizar operadores lógicos (and, or, not). Desarr
 también un programa para ingresar los tres valores, invocar a la función y mostrar
 el máximo hallado, o un mensaje informativo si éste no existe."""
 
-def mayor (a,b,c):
-    if a>b:
-        if a>c:
-            return a
-    if b>a:
-        if b>c:
-            return b
-    if c>a:
-        if c>b:
-            return c
-    return -1
+def encontrar_mayor(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    elif c>a and c>b:
+        return c
+    else:
+        return -1
 
 def ingresar_valores():
     primero = int(input("Ingrese el primer número: "))
@@ -35,11 +33,10 @@ def ingresar_valores():
         else:
             bien=True
 
-    resultado = mayor(primero, segundo, tercero)
+    resultado = encontrar_mayor(primero, segundo, tercero)
 
     if resultado == -1:
         print("No existe un mayor único.")
     else:
         print(f"El mayor único es: {resultado}")
-
 ingresar_valores()

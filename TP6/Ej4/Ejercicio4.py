14a8dd4a6d1448ed09d929e88189ce5ed34a659d
Alexis-9 <alexis.munoz.9u@gmail.com>
2025-11-13 23:39:57 -0300

    Tp6

---
+++ TP6/Ej4/Ejercicio4.py
@@ -0,0 +1,55 @@
+"""Desarrollar un programa para eliminar todos los comentarios de un programa escrito en lenguaje Python. Tener en cuenta que los comentarios comienzan con el
+signo # (siempre que éste no se encuentre encerrado entre comillas simples o dobles) y que también se considera comentario a las cadenas de documentación
+(docstrings).
+"""
+def eliminar_comentarios():
+    programa="prueba.txt" #input("Ingrese el nombre del programa de python: ")
+    with open(programa, "r", encoding="utf-8") as archivo:
+        lineas=archivo.readlines()
+
+
+    comentario=False
+    entre_comillas=False
+
+    nuevas_lineas=[]
+    linea_nueva=""
+
+    sucesion=0
+
+
+    for linea in lineas:
+        for caracter in linea:
+
+            if caracter=='"' or caracter== "'":
+                entre_comillas = True
+
+            if caracter=="#" and not entre_comillas :
+                comentario=True
+
+            if caracter=='"':
+                sucesion+=1
+                if sucesion==3:
+                    comentario=True
+            else:
+                sucesion=0
+
+            if not comentario and not sucesion:
+                linea_nueva+=caracter
+
+
+
+
+        comentario=False
+        entre_comillas=False
+        sucesion=0
+        if linea_nueva.strip():
+            nuevas_lineas.append("".join(linea_nueva).strip())
+        linea_nueva=""
+
+
+    #Lo hice en otro archivo asi se ve la diferencía pero se podría hacer en el mismo archivo para que lo sobreescriba
+    with open(f"{programa}_1", "w", encoding="utf-8") as archivo:
+        for linea in nuevas_lineas:
+            archivo.write(f"{linea}\n")
+
+eliminar_comentarios()
